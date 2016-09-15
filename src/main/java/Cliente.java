9b460a0b9236ebf04d5c5f2b26b9e2d73f6c9538
yrocha <yrocha@azi.com.br>
2016-09-15 13:03:13 -0400

    commit 2

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -1,30 +1,61 @@
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
 /**
  * Created by yrocha on 15/09/2016.
  */
-public class Cliente {
+public class Cliente implements Runnable {
+
+    private Socket cliente;
+
+    public Cliente(Socket cliente) {
+        this.cliente = cliente;
+    }
+
     public static void main(String[] args) throws IOException {
-        Socket cliente = new Socket("127.0.0.1", 9080);
-        if (cliente.isConnected()) {
-            System.out.println("Conectado");
-        }
-        PrintStream saida = new PrintStream(cliente.getOutputStream());
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Para sair do chat digite 0!");
-        while (teclado.hasNextLine()) {
-            if (!teclado.nextLine().equals("0")) {
-                saida.println(teclado.nextLine());
-            } else {
-                System.out.println("Você saiu do chat!");
-                break;
+        Socket socket = new Socket("127.0.0.1", 9080);
+        Cliente c = new Cliente(socket);
+        Thread t = new Thread(c);
+        t.start();
+    }
+
+    public void run() {
+        try {
+            if (this.cliente.isConnected()) {
+                System.out.println("Conectado ");
             }
+
+            //ENVIA MENSAGEM
+            OutputStream os = cliente.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+
+            Scanner teclado = new Scanner(System.in);
+            PrintStream saida = new PrintStream(this.cliente.getOutputStream());
+
+            String mensagem = "";
+            while (teclado.hasNextLine()) {
+                mensagem = teclado.nextLine();
+            }
+            bw.write(mensagem);
+            bw.flush();
+            System.out.println("A mensagem enviada ao servidor foi: " + mensagem);
+
+            //RECEBE MENSAGEM
+            InputStream is = cliente.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String message = br.readLine();
+            System.out.println("Message received from the server : " +message);
+
+
+            saida.close();
+            teclado.close();
+            cliente.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        saida.close();
-        teclado.close();
-        cliente.close();
     }
 }
--- src/main/java/Servidor.java
+++ src/main/java/Servidor.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -6,20 +6,57 @@ import java.util.Scanner;
 /**
  * Created by yrocha on 15/09/2016.
  */
-public class Servidor {
+public class Servidor implements Runnable {
+
+    public Socket cliente;
+
+    public Servidor(Socket cliente) {
+        this.cliente = cliente;
+    }
+
     public static void main(String[] args) throws IOException {
         ServerSocket servidor = new ServerSocket(9080);
         System.out.println("Esperando conexão...");
-        Socket cliente = servidor.accept();
-        if (cliente.isConnected()) {
-            System.out.println("Cliente conectado! " + cliente.getInetAddress().getHostAddress());
+
+        while (true) {
+            Socket cliente = servidor.accept();
+            // Cria uma thread do servidor para tratar a conexão
+            Servidor tratamento = new Servidor(cliente);
+            Thread t = new Thread(tratamento);
+            // Inicia a thread para o cliente conectado
+            t.start();
         }
-        Scanner scanner = new Scanner(cliente.getInputStream());
-        while (scanner.hasNextLine()) {
-            System.out.println(scanner.nextLine());
+
+    }
+
+    public void run() {
+        try {
+            InputStream is = cliente.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String mensagem = br.readLine();
+            System.out.println("A mensagem recebida do cliente foi: " + mensagem);
+
+            OutputStream os = cliente.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bfw = new BufferedWriter(osw);
+            bfw.write(mensagem);
+            bfw.flush();
+
+
+//            System.out.println("Nova conexao com o cliente " + this.cliente.getInetAddress().getHostAddress());
+//            Scanner scanner = new Scanner(cliente.getInputStream());
+//
+//            while (scanner.hasNextLine()) {
+//                System.out.println(scanner.nextLine());
+//            }
+//
+//            System.out.println("Conexão com cliente: " + this.cliente.getInetAddress().getHostAddress() + " encerrada!");
+
+            //scanner.close();
+            cliente.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        scanner.close();
-        cliente.close();
-        servidor.close();
     }
 }
